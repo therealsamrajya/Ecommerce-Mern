864aafc54588626977df9fa5581d5ba28dbb5e3b
Samrajya <97937630+therealsamrajya@users.noreply.github.com>
2024-06-14 20:26:44 +0545

    admin pannel complete

--- admin/src/components/ListProduct/ListProduct.jsx
+++ admin/src/components/ListProduct/ListProduct.jsx
@@ -20,12 +20,14 @@ const ListProduct = () => {
 
   const removeProduct = async (id) => {
     try {
-      const response = await fetch(
-        `http://localhost:4000/removeproduct/${id}`,
-        {
-          method: "DELETE",
-        }
-      );
+      const response = await fetch(`http://localhost:4000/removeproduct`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -40,10 +42,10 @@ const ListProduct = () => {
   }, []);
 
   return (
-    <div>
-      <h1>All Products List</h1>
+    <div className="flex flex-col items-center w-[100%] h-[740px] px-[10px] py-[50px] rounded-md bg-white">
+      <h1 className="text-2xl mb-[20px]">All Products List</h1>
       {error && <p style={{ color: "red" }}>Error: {error}</p>}
-      <div>
+      <div className="grid grid-cols-[1fr_3fr_1fr_1fr_1fr_1fr] gap-[10px] w-[100%] px-[20px] py-[0px] text-[#454545] text-[18px] font-semibold max-sm:text-xs">
         <p>Products</p>
         <p>Title</p>
         <p>Old Price</p>
@@ -53,8 +55,11 @@ const ListProduct = () => {
       </div>
       <hr />
       {allproducts.map((product, index) => (
-        <div key={index}>
+        <div
+          className="items-center font-semibold grid grid-cols-[1fr_3fr_1fr_1fr_1fr_1fr] gap-[15px] w-[100%] px-[20px] py-[20px] text-[#454545] max-sm:text-[13px] text-[18px]"
+          key={index}>
           <img
+            className="h-[80px]"
             src={product.image}
             alt={product.name}
             style={{ width: "50px", height: "50px", objectFit: "cover" }}
@@ -63,7 +68,12 @@ const ListProduct = () => {
           <p>${product.old_price}</p>
           <p>${product.new_price}</p>
           <p>{product.category}</p>
-          <img src={cross_icon} />
+          <img
+            onClick={() => removeProduct(product.id)}
+            className="cursor-pointer m-auto"
+            src={cross_icon}
+            alt="Remove"
+          />
         </div>
       ))}
     </div>
